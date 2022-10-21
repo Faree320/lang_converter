6819a1497e4bf9d5560388586b07acfe007c2b4c
Faree320 <100018647+Faree320@users.noreply.github.com>
2022-10-21 11:57:46 +0500

    Hero

--- api/views.py
+++ api/views.py
@@ -11,26 +11,29 @@ def translator_func(request):
     if request.method == "POST":
         data = json.loads(request.body)
         chunk = data["name"]
-        converter = []
-        checker = []
+        src = data["inputDest"]['value']
+        routes_data = []
         for dest in data["languages"]:
             print(dest["value"])
             print(data)
             translator = Translator()
-            translation = translator.translate(chunk, dest=dest["value"])
-            converter.append(translation.text)
+            translation = translator.translate(chunk, src=src, dest=dest["value"])
+            routes_trans = translation.text
             print(translation.text)
 
             blob = TextBlob(translation.text)
             try:
                 final = blob.translate(from_lang=dest["value"], to='en')
-                checker.append(str(final))
+                routes_cross_check = str(final)
+                print(routes_cross_check)
             except NotTranslated:
-                checker.append(str(translation.text))
-        routes = {
-            "Translation": converter,
-            "Cross Check": checker,
-        }
+                routes_cross_check = str(translation.text)
+            routes_data.append({
+                "translation": routes_trans,
+                "cross_check": routes_cross_check,
+                "language": dest["label"]
+            })
+        routes = routes_data
         # print(LANGUAGES)
         return JsonResponse(routes, safe=False)
 
